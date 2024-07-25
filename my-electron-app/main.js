39d130cf68dc645bb75b960b7ee8dd4569b7c599
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-07-25 14:52:26 -0400

    created preload.js and created new function

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,1 +1,20 @@
-console.log("Hello from Main.js")
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+
+const createMainWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 500,
+        height: 800,
+    })
+
+    mainWindow.loadFile('./app/index.html')
+}
+
+app.whenReady().then(() => {
+    createMainWindow()
+    
+    app.on('window-all-closed', () => {
+        if(process.platform !== 'darwin') app.quit()
+    })
+})
